021591c1dbeb0b380a65bb6b80bb46633bae3709
Amazingmercy <amaefulemercy499@gmail.com>
2024-01-29 04:49:53 +0000

    Modified swagger docs

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,14 +6,15 @@ const secretKey = process.env.JWT_SECRET_KEY
 
 const authenticate = async(req, res, next) => {
     const token = req.headers.authorization
-    if(!token || !token.startsWith('Bearer')){
+    if(!token){
         throw new UnAuthenticatedError('user is not logged in')
     }
 
-    const decodedToken = token.split(' ')[1]
+    //const decodedToken = token.split(' ')[1]
+    const payload = jwt.verify(token.replace("Bearer ", ""), secretKey);
     
     try{
-        const payload = jwt.verify(decodedToken, secretKey)
+        //const payload = jwt.verify(decodedToken, secretKey)
         req.user = payload
         next()
     } catch (error){
